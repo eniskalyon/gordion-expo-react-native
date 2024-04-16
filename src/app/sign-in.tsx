e7dff3124dfa4c2429afeb5ae2cc752084f84580
Enis Kalyon <fzlnsklyn@gmail.com>
2024-04-16 15:53:39 +0100

    begin building sign in screen

---
+++ src/app/sign-in.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native"
+
+
+export default function SignIn() {
+    return (
+        <View>
+            <Text>Sign in with Google</Text>
+        </View>
+    )
+}
+
